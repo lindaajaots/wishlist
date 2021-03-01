886a5be91a409863ea2577bc894d767ba4b9c5a8
lindaajaots <linda.ajaots@gmail.com>
2021-03-01 17:40:43 +0200

    töötab, vaja vaadata login, logout ja wishlisti näeb kui ei ole sisse logitud

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,23 @@
 class UsersController < ApplicationController
 
-
   def index
     @users = User.all
   end
 
+  def show
+  end
+
+  def new
+  end
+
+  def edit
+  end
+
+  def destroy
+    @user.destroy
+    respond_to do |format|
+      format.html { redirect_to users_url, notice: "User was deleted." }
+    end
+  end
 
 end
